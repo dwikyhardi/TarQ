bde6a9b9404b670a855526c7fcd96250bcc819d3
kazehiro <adriancodyokoteam@gmail.com>
2018-12-12 16:33:53 +0700

    Find Guru

---
+++ app/src/main/java/root/example/com/tar_q/Find_Guru.java
@@ -0,0 +1,90 @@
+package root.example.com.tar_q;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Find_Guru extends AppCompatActivity {
+
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private String userID;
+
+
+    private ListView mListViewGuru;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find__guru);
+
+        mListViewGuru = (ListView) findViewById(R.id.listViewGuru);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference().child("TARQ").child("USER").child("GURU");
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                showData((Map<String, Object>) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void showData(Map<String, Object> dataSnapshot) {
+
+        final ArrayList<String> Nama = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map nama = (Map) entry.getValue();
+            Nama.add((String) nama.get("nama"));
+        }
+        final ArrayList<String> Id_penerima = new ArrayList<>();
+        for (Map.Entry<String, Object> entry : dataSnapshot.entrySet()) {
+            Map id_penerima = (Map) entry.getValue();
+            Id_penerima.add((String) id_penerima.get("id_user"));
+        }
+            mListViewGuru.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    toastMessage(Nama.get(position));
+                }
+            });
+            ArrayAdapter namaGuru = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Nama);
+            mListViewGuru.setAdapter(namaGuru);
+        }
+
+    public void toastMessage(String message){
+        Toast.makeText(this, message,Toast.LENGTH_SHORT).show();
+    }
+
+}
