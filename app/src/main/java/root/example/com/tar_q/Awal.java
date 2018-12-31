2288a431f6a13221ee007b9010fa492e98680328
chairilrafi11 <43770375+chairilrafi11@users.noreply.github.com>
2018-12-31 10:20:20 +0700

    Cauman edan | Layout Awal

--- app/src/main/java/root/example/com/tar_q/Awal.java
+++ app/src/main/java/root/example/com/tar_q/Awal.java
@@ -1,13 +1,37 @@
 package root.example.com.tar_q;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Awal extends AppCompatActivity {
+
+    private Button BtnSignInAwal, BtnSignUpAwal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_awal);
+
+        BtnSignInAwal = (Button) findViewById(R.id.btnSignInAwal);
+        BtnSignUpAwal = (Button) findViewById(R.id.btnSignUpAwal);
+
+        BtnSignUpAwal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Awal.this, register.class);
+                startActivity(mIntent);
+            }
+        });
+
+        BtnSignInAwal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Awal.this, MainActivity.class);
+                startActivity(mIntent);
+            }
+        });
     }
 }
