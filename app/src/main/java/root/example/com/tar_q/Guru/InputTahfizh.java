b84b4f55cdde7c55b1a9249e4cbf83bbe9e938e3
dwikyhardi <dwikyhardiansah93@gmail.com>
2018-12-31 13:38:21 +0700

    InputForm

---
+++ app/src/main/java/root/example/com/tar_q/Guru/InputTahfizh.java
@@ -0,0 +1,39 @@
+package root.example.com.tar_q.Guru;
+
+public class InputTahfizh {
+    public String id_guru,
+            id_jamaah,
+            nama_guru,
+            nama_jamaah,
+            tanggal_belajar,
+            pertemuan_ke,
+            setoran,
+            murajaah,
+            hafalanbaru,
+            keterangan;
+
+    public InputTahfizh(
+            String id_guru,
+            String id_jamaah,
+            String nama_guru,
+            String nama_jamaah,
+            String tanggal_belajar,
+            String pertemuan_ke,
+            String setoran,
+            String murajaah,
+            String hafalanbaru,
+            String keterangan
+    ) {
+        this.id_guru = id_guru;
+        this.id_jamaah = id_jamaah;
+        this.nama_guru = nama_guru;
+        this.nama_jamaah = nama_jamaah;
+        this.tanggal_belajar = tanggal_belajar;
+        this.pertemuan_ke = pertemuan_ke;
+        this.setoran = setoran;
+        this.murajaah = murajaah;
+        this.hafalanbaru = hafalanbaru;
+        this.keterangan = keterangan;
+
+    }
+}
---
+++ app/src/main/java/root/example/com/tar_q/Guru/InputTahsinDanBahasaArab.java
@@ -0,0 +1,41 @@
+package root.example.com.tar_q.Guru;
+
+public class InputTahsinDanBahasaArab {
+    public String id_guru,
+            id_jamaah,
+            nama_guru,
+            nama_jamaah,
+            tanggal_belajar,
+            pertemuan_ke,
+            materi,
+            pencapaian,
+            kendala,
+            solusi,
+            keterangan;
+
+    public InputTahsinDanBahasaArab(String id_guru,
+                                    String id_jamaah,
+                                    String nama_guru,
+                                    String nama_jamaah,
+                                    String tanggal_belajar,
+                                    String pertemuan_ke,
+                                    String materi,
+                                    String pencapaian,
+                                    String kendala,
+                                    String solusi,
+                                    String keterangan) {
+
+        this.id_guru = id_guru;
+        this.id_jamaah = id_jamaah;
+        this.nama_guru = nama_guru;
+        this.nama_jamaah = nama_jamaah;
+        this.tanggal_belajar = tanggal_belajar;
+        this.pertemuan_ke = pertemuan_ke;
+        this.materi = materi;
+        this.pencapaian = pencapaian;
+        this.kendala = kendala;
+        this.solusi = solusi;
+        this.keterangan = keterangan;
+
+    }
+}
