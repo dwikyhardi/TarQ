d86bfc32bc9ec6b60657d3269036bf15cbf03ef6
chairilrafi11 <43770375+chairilrafi11@users.noreply.github.com>
2018-12-31 13:29:59 +0700

    Cauman edan | Layout Form Bahasa Arab

---
+++ app/src/main/java/root/example/com/tar_q/Form_Bahasa_Arab.java
@@ -0,0 +1,13 @@
+package root.example.com.tar_q;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Form_Bahasa_Arab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_bahasa_arab);
+    }
+}
