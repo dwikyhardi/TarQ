c468c551f816d83148980bc4880ac4b34de93bf1
chairilrafi11 <43770375+chairilrafi11@users.noreply.github.com>
2019-04-22 14:22:19 +0700

    Fix Button Google & Padding

--- app/src/main/java/root/example/com/tar_q/MainActivity.java
+++ app/src/main/java/root/example/com/tar_q/MainActivity.java
@@ -68,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
     // UI references.
     private EditText mEmail, mPassword;
     private Button btnSignIn;
-    private Button btnGoogle;
     private FirebaseAuth firebaseAuth;
     public static final int REQUEST_CODE_CAMERA_FOTO = 0020;
     public static final int REQUEST_CODE_GPS_FINE = 7171;
@@ -85,20 +84,8 @@ public class MainActivity extends AppCompatActivity {
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
         btnSignIn = (Button) findViewById(R.id.email_sign_in_button);
-        btnGoogle = (Button) findViewById(R.id.btnGoogle);
         mAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
-        btnGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*startActivityForResult(
-                        AuthUI.getInstance().createSignInIntentBuilder()
-                                .setIsSmartLockEnabled(true)
-                                .build(),LOGIN_PERMISSION);*/
-                showSnackbar(v, "Layanan Belum Tersedia", 3000);
-
-            }
-        });
 
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
